7b10f1e61df1ac51aeff6281c51aa5e1f32349bf
Nicolas Conguisti <nicolas.conguisti1@gmail.com>
2024-11-24 22:31:50 +0100

    Ajout de la map. Stade actuel : Un niveau défile avec une map finie. pas de collision

--- main.c
+++ main.c
@@ -8,8 +8,13 @@
 const int WINDOW_WIDTH = 1200;
 const int WINDOW_HEIGHT = 800;
 
-const int RECT_WIDTH = 50;
-const int RECT_HEIGHT = 70;
+const int CHARACTER_WIDTH = 50;
+const int CHARACTER_HEIGHT = 70;
+const int CHARACTER_SPEED = 40;
+
+const int MAP_WIDTH = 5000;
+const int MAP_HEIGHT = 200;
+const int MAP_SPEED = 2;
 
 //Type Character
 typedef struct {
@@ -23,9 +28,19 @@ typedef struct {
 
 } Character;
 
+//Type Map
+typedef struct {
+    int x;
+	int y;
+    int width;
+	int height;
+	SDL_Color color;
 
-//Fonction appelée à chaque frame, elle actualise la fenetre
-void updateCharacter(SDL_Renderer * renderer, Character character){
+} Map;
+
+
+//Fonction appelée à chaque frame, elle actualise la fenetre en faisant apparaitre les éléments
+void update(SDL_Renderer * renderer, Map map, Character character){
 
 	//Créer la fenetre
 	SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
@@ -33,18 +48,29 @@ void updateCharacter(SDL_Renderer * renderer, Character character){
 
 	//Faire apparaitre le character
 	SDL_SetRenderDrawColor(renderer, character.color.r, character.color.g, character.color.b, character.color.a);
-	SDL_Rect rect = { character.x, character.y, character.width, character.height }; // x, y, largeur, hauteur
-	SDL_RenderFillRect(renderer, &rect);
+	SDL_Rect rectCharacter = { character.x, character.y, character.width, character.height }; // x, y, largeur, hauteur
+	SDL_RenderFillRect(renderer, &rectCharacter);
+
+	//Faire apparaitre la map
+	SDL_SetRenderDrawColor(renderer, map.color.r, map.color.g, map.color.b, map.color.a);
+	SDL_Rect rectMap = { map.x, map.y, map.width, map.height }; // x, y, largeur, hauteur
+	SDL_RenderFillRect(renderer, &rectMap);
 }
 
-//Fonction qui fait bouger le character
+//Fonction qui fait bouger automatiquement la map
+void movePositionMap(Map * map){
+
+	map->x -= MAP_SPEED;
+}
+
+//Fonction qui fait bouger automatiquement le character
 void movePositionCharacter(Character * character){
 
 	// changement de direction lorsque le character sort de la fenetre
-	if(character->x > WINDOW_WIDTH - RECT_WIDTH){
+	if(character->x > WINDOW_WIDTH - CHARACTER_WIDTH){
 		character->way_x = 2;
 	}
-	if(character->y > WINDOW_HEIGHT - RECT_HEIGHT){
+	if(character->y > WINDOW_HEIGHT - CHARACTER_HEIGHT){
 		character->way_y = 2;
 	}
 	if(character->x < 0){
@@ -56,17 +82,17 @@ void movePositionCharacter(Character * character){
 
 	//Avancement de x et y dans la direction indiquée
 	if(character->way_x == 1){
-		character->x += 2;
+		character->x += CHARACTER_SPEED;
 	}
 	else{
-		character->x -= 2;
+		character->x -= CHARACTER_SPEED;
 	}
 
 	if(character->way_y == 1){
-		character->y += 2;
+		character->y += CHARACTER_SPEED;
 	}
 	else{
-		character->y -= 2;
+		character->y -= CHARACTER_SPEED;
 	}
         
 }
@@ -84,19 +110,19 @@ void verifyEventKeyPressed(SDL_Event event, bool * running, Character * characte
 		switch (event.key.keysym.sym) {
 
 			case SDLK_LEFT:
-				character->x -= 20;
+				character->x -= CHARACTER_SPEED;
 				break;
 
 			case SDLK_RIGHT:
-				character->x += 20;
+				character->x += CHARACTER_SPEED;
 				break;
 
 			case SDLK_UP:
-				character->y -= 20;
+				character->y -= CHARACTER_SPEED;
 				break;
 
 			case SDLK_DOWN:
-				character->y += 20;
+				character->y += CHARACTER_SPEED;
 				break;
 
 			case SDLK_ESCAPE:
@@ -124,9 +150,13 @@ int main(int argc, const char * argv[]) {
 	bool running = true;
 	SDL_Event event;
 
+	//Initialisation de la Map
+	SDL_Color colorMap = {0, 255, 0, 255};
+	Map map = {0, 500, MAP_WIDTH, MAP_HEIGHT, colorMap};  // x, y, width, height, color
+
 	//Initialisation du character
-	SDL_Color color = {255, 0, 0, 255};
-	Character character = {0, 0, RECT_WIDTH, RECT_HEIGHT, 1, 1, color};  // x, y, width, height, way_x, way_y, color
+	SDL_Color colorCharacter = {255, 0, 0, 255};
+	Character character = {0, 0, CHARACTER_WIDTH, CHARACTER_HEIGHT, 1, 1, colorCharacter};  // x, y, width, height, way_x, way_y, color
 
 	while(running){
 		
@@ -135,8 +165,10 @@ int main(int argc, const char * argv[]) {
 			verifyEventKeyPressed(event, &running, &character);
 		}
 
-		movePositionCharacter(&character);
-		updateCharacter(renderer, character);
+		//movePositionCharacter(&character);
+
+		movePositionMap(&map);
+		update(renderer, map, character);
 
 		SDL_RenderPresent(renderer);
 	}
